c84a37e601c2adc624696205163808aec161c3a6
mhy-web <dsz9258@163.com>
2018-02-01 12:24:19 +0800

    polyfill

--- dialog/main.js
+++ dialog/main.js
@@ -1,14 +1,17 @@
+import dialogPolyfill from 'dialog-polyfill'
+const dialog = document.querySelector('dialog')
+
+dialogPolyfill.registerDialog(dialog);
+
 (function() {
   var updateButton = document.getElementById('updateDetails');
   var cancelButton = document.getElementById('cancel');
   var favDialog = document.getElementById('favDialog');
 
-  // Update button opens a modal dialog
   updateButton.addEventListener('click', function() {
     favDialog.showModal();
   });
 
-  // Form cancel button closes the dialog box
   cancelButton.addEventListener('click', function() {
     favDialog.close();
   });
