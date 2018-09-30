3e92f3073e2df53ddc7be501a2292971920ac367
mhy_Mac_client <dsz9258@163.com>
2018-09-30 11:53:46 +0800

    fs

--- nodejs/fs_model.js
+++ nodejs/fs_model.js
@@ -4,16 +4,16 @@ const path = require('path');
 // console.log('__dirname', __dirname);
 // console.log('__filename', __filename);
 
-let readSyncStr = fs.readFileSync(path.join(__dirname, 'beReadFile.json')).toString()
+let readSyncStr = fs.readFileSync( path.join( __dirname, 'beReadFile.json' )).toString()
 // console.log(readSyncStr);
 
-fs.readFile(path.join(__dirname, './beReadFile.json'), 'utf8', (err, data) => {
+fs.readFile( path.join( __dirname, './beReadFile.json' ), 'utf8', ( err, data ) => {
 	if (err) {
 		throw err;
 	}
 	// console.log(data)
 })
-fs.writeFile(path.join(__dirname, 'beReadFile.json'), 'second.md', (err) => {
+fs.writeFile( path.join( __dirname, 'beReadFile.json' ), 'second.md', ( err ) => {
 	if(err) throw err;
 })
 
