4385c6db3a670d9659f5a52fae65c0bba69136e6
mhy_Mac_client <dsz9258@163.com>
2018-11-07 17:17:15 +0800

    feat: use espress to mook data

--- mook/server.js
+++ mook/server.js
@@ -1,3 +1,49 @@
-import express from 'express';
+let express = require('express');
+let fs = require('fs');
+let app = express();
 
+const routes = [
+	{
+		name: 'home',
+		type: 'get',
+		url: '/home'
+	}, {
+		name: 'job',
+		type: 'get',
+		url: '/job'
+	}
+]
 
+app.get('/index.html', function(req, res) {
+  res.sendFile(__dirname + req.path);
+});
+
+/*
+app.get('/home', function(req, res) {
+	res.setHeader('Content-Type', 'application/json;charset=utf-8');
+    fs.readFile('./home.json', function(err, data) {
+			if(err) {
+				throw err;
+			}
+			res.json(JSON.parse(data));
+		})
+})
+*/
+
+exports.routes = routes;
+
+routes.forEach((route, index) => {
+	let name = route.name;
+
+	app[route.type](route.url, function(req, res) {
+		res.setHeader('Content-Type', 'application/json;charset=utf-8');
+		fs.readFile('./' + name + '.json', function(err, data) {
+			if(err) throw err;
+			res.json(JSON.parse(data));
+		})
+	})
+})
+
+app.listen(4003, () =>  {
+    console.log('app start at port 4003.....');
+})
