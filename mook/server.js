6f3fc5948c9b7769e701ccff7c32cdeec67e1cf2
mhyuan <dsz9258@126.com>
2018-11-13 21:42:22 +0800

    feat: mook data server

--- mook/server.js
+++ mook/server.js
@@ -33,9 +33,10 @@ app.get('/home', function(req, res) {
 // exports.routes = routes;
 
 routes.forEach((route, index) => {
-	let name = route.name;
+	let { name, path } = route;
 
 	app[route.type](route.url, function(req, res) {
+		res.setHeader('Access-Control-Allow-Origin', '*');
 		res.setHeader('Content-Type', 'application/json;charset=utf-8');
 		fs.readFile('./' + name + '.json', function(err, data) {
 			if(err) throw err;
@@ -47,3 +48,31 @@ routes.forEach((route, index) => {
 app.listen(4003, () =>  {
     console.log('app start at port 4003.....');
 })
+
+
+// routes.forEach((route, index) => {
+// 	let { name, path } = route;
+
+// 	app.get(path, function(req, res) {
+// 			res.setHeader('Access-Control-Allow-Origin', '*');
+
+// 			if(req.method === 'OPTIONS') {
+// 					res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,DELETE');
+// 					res.setHeader('Access-Control-Allow-Headers', 'origin, x-requested-with, content-type, accept');
+// 					res.setHeader('Content-Type', 'application/json;charset=utf-8');
+// 					res.setHeader('Access-Control-Allow-Credentials', true);
+// 			}
+
+// 			fs.readFile('./mook/' + name + '.json', function(err, data) {
+// 					if(err) throw err;
+// 					console.log(`get request from ${path}, response: ${data}`);
+
+// 					res.json(JSON.parse(data));
+// 			})
+// 	})
+// })
+
+// app.listen(PORT, () => {
+// 	console.log(`mook server start at prot ${PORT}.....`);
+// })
+
