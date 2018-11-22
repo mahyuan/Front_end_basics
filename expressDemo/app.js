a0e16280bd9ca7d5cbe5fb364075ad446dea7042
mhyuan <dsz9258@126.com>
2018-11-22 18:06:17 +0800

    Feat: express test

--- expressDemo/app.js
+++ expressDemo/app.js
@@ -1,7 +1,39 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
-
 app.get('/', (req, res) => res.send('hello'))
+app.get('/api/students', (req, res) => {
+	let students = [
+		{
+			id: 1,
+			name: 'john',
+			classRoomId: 1
+		},
+		{
+			id: 2,
+			name: 'bob',
+			classRoomId: 1
+		},
+		{
+			id: 3,
+			name: 'lee',
+			classRoomId: 1
+		},
+		{
+			id: 4,
+			name: 'jack',
+			classRoomId: 1
+		},
+	]
+	res.send(students)
+})
+
+app.get('/api/courses', async function(req, res) {
+	let { studentID } = req.query
+	let students = await axios.get('/api/students')
+	console.log('students', students);
+	res.send(students)
+})
 
 app.listen(2000, () => console.log('Listen port 2000'))
 
