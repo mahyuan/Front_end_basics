f509fe8c370ec5c56daed03020ee7e37e9888190
mhyuan <dsz9258@126.com>
2020-03-02 15:41:21 +0800

    vue ssr

--- vue-ssr/src/server.js
+++ vue-ssr/src/server.js
@@ -5,20 +5,21 @@ const express = require('express');
 const Vue = require('vue');
 const fs = require('fs');
 const path = require('path');
-const renderer = require('vue-server-renderer').createRenderer();
 const server = express();
 const root = require('../config').root;
 const filePath = path.join(root, 'src/template')
 
+const renderer = require('vue-server-renderer').createRenderer({
+  template: fs.readFileSync(path.join(filePath, 'index.html'), 'utf-8')
+});
+
 server.get('*', (req, res) => {
-  console.log('root', root);
-  console.log('cwd', process.cwd());
   const app = new Vue({
     data: {
+      msg: 'vue ssr',
       url: req.url
     },
-    // template: `<div>hello, {{ url }} </div>`
-    template: fs.readFileSync(path.join(filePath, 'index.template.html'), 'utf-8')
+    template: `<div>{{msg}}</div>`
   });
 
   renderer.renderToString(app, (err, html) => {
@@ -26,19 +27,10 @@ server.get('*', (req, res) => {
       res.status(500).end('Internal Server Error');
       return;
     }
-
-    res.end(`
-      <!DOCTYPE>
-      <html>
-        <head>
-          <title>hello</title>
-        </head>
-        <body>
-          ${html}
-        </body>
-      </html>
-    `)
+    console.log('html', html);
+    res.end(html)
   })
 });
 
 server.listen(9111);
+console.log('server listen on: http://127.0.0.1:9111')
